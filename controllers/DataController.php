0af8fa539166f4345833dd11152b648c878ebce5
iogami <iogami.raito@gmail.com>
2025-08-14 15:56:24 +0300

    finish

--- controllers/DataController.php
+++ controllers/DataController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use Yii;
 use yii\web\Controller;
+use yii\web\BadRequestHttpException;
 use app\services\DataService;
 
 class DataController extends Controller
@@ -15,17 +16,27 @@ class DataController extends Controller
 
     public function actionSubthemesList()
     {
-        $themeId = (int)Yii::$app->request->get('theme_id', 1);
-        $themeId = ($themeId < 1) ? 1 : $themeId;
-
+        $themeId = $this->getPositiveIntParam('theme_id');
         return $this->asJson(DataService::subthemesList($themeId));
     }
 
     public function actionSubthemeText()
     {
-        $subthemeId = (int)Yii::$app->request->get('subtheme_id', 1);
-        $subthemeId = ($subthemeId < 1) ? 1 : $subthemeId;
-
+        $subthemeId = $this->getPositiveIntParam('subtheme_id');
         return $this->asJson(['text' => DataService::subthemeText($subthemeId)]);
+    }
+
+    /**
+     * Получает положительный целочисленный GET-параметр
+     *
+     * @param string $name
+     * @param int $default
+     * @return int
+     */
+    protected function getPositiveIntParam(string $name, int $default = 1): int
+    {
+        $value = (int)Yii::$app->request->get($name, $default);
+
+        return ($value < 1) ? 1 : $value;
     }
 }
